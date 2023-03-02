661009e9ae42ad52424dc883510da45fdf97df52
quantium <alexcollsoutumuro@gmail.com>
2023-03-02 14:40:24 +0100

    add ScreenPanel

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,67 @@
 
 <script lang="ts">
+
 import { defineComponent } from 'vue';
-import CalculatorPanel from './components/CalculatorPanel.vue';
+import useStore from './services/store';
+import ScreenPanel from './components/ScreenPanel.vue';
+import KeyboardPanel from './components/KeyboardPanel.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    CalculatorPanel
+    ScreenPanel,
+    KeyboardPanel,
+  },
+  setup() {
+    const store = useStore();
+    return {
+      store
+    }
   }
 });
+
 </script>
 
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <calculator-panel />
+
+  <div class="pt-4 h-screen w-screen"
+    :class="store.dark ?
+    'bg-gray-900 shadow-gray-700 border-gray-200 text-gray-200' :
+    'bg-gray-100 shadow-gray-300 border-gray-900 text-gray-800'">
+    <div class="flex justify-center align-middle p-4">
+      <img alt="Vue logo" src="./assets/logo.png">
+    </div>
+    <screen-panel class="p-8" />
+    <keyboard-panel class="p-8" />
   </div>
+
 </template>
-<style lang="less">
+
+<style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+¡
+html {
+  overflow-x: hidden;
+}
+::-webkit-scrollbar {
+  width: 3px;
+  height: 3px;
+  background-color: transparent !important;
+}
+::-webkit-scrollbar-track {
+  background-color: rgba(179, 179, 179, 0.692);
+  border-radius: 1rem;
+  margin: 0.4rem;
+  margin-top: 0.4rem;
+  border-left: 1px solid rgba(211, 211, 211, 0);
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 20px;
+  background:  linear-gradient(30deg, #1bd3247c, #21b913be);
+  box-shadow: inset 0 0 6px #30d31b,
+}
+
 </style>
---
+++ client/src/components/ScreenPanel.vue
@@ -0,0 +1,62 @@
+<script lang="ts">
+// import { ref } from 'vue';
+import useStore from '../services/store';
+// import click_sound from '../../assets/sounds/beep.mp3';
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      number: 0
+    }
+  },
+  setup () {
+    const store = useStore();
+    const nf = Intl.NumberFormat();
+
+    return {
+      store,
+      nf
+     
+    }
+  },
+  methods: {
+    clickNum(num:number): void {
+      console.log(num);
+      // TODO
+    },
+    resetNum(): void {
+      // TODO
+    },
+    deleteNum(): void {
+      // TODO
+    }
+  }
+}
+</script>
+<template>
+  <div class="m-auto w-[420px] p-2 mt-0">
+    <div class="text-2xl py-1 text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-green-600">
+      <a class="text-xl mr-5 ">
+        ⛏️ CALCULATE 
+      </a>
+      {{ nf.format(Number(number)).replaceAll(',', ' ') }}
+    </div>
+    <div class="grid grid-cols-3 gap-1 text-s font-semibold text-center py-4 px-6 rounded-xl">num
+      <button @click="clickNum(1)" :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-4 px-2 rounded-tl-xl align-middle relative border">1</button>
+      <button @click="clickNum(2)" :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-4 px-2 align-middle relative border">2</button>
+      <button @click="clickNum(3)" :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-4 px-2 rounded-tr-xl align-middle relative border">3</button>
+      <button @click="clickNum(4)" :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-4 px-2 align-middle relative border">4</button>
+      <button @click="clickNum(5)" :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-4 px-2 align-middle relative border">5</button>
+      <button @click="clickNum(6)" :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-4 px-2 align-middle relative border">6</button>
+      <button @click="clickNum(7)" :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-4 px-2 rounded-bl-xl align-middle relative border">7</button>
+      <button @click="clickNum(8)" :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-4 px-2 align-middle relative border">8</button>
+      <button @click="clickNum(9)" :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-4 px-2 rounded-br-xl align-middle relative border">9</button>
+      <button @click="resetNum()"  :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-2 px-2 rounded-3xl m-3 align-middle relative border">↻</button>
+      <button @click="clickNum(0)" :class="store.dark ? 'bg-gray-600 hover:bg-gray-400 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100'" class="py-4 px-2 rounded-b-xl align-middle relative border">0</button>
+      <button @click="deleteNum()" :class="store.dark ? 'bg-gray-600 hover:bg-green-600' : 'bg-gray-50 hover:bg-green-600'" class="py-2 px-2 rounded-3xl m-3 align-middle relative border border-green-600">⛏️</button>          
+    </div>
+  </div>
+</template>
