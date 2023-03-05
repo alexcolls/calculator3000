fec3fbde3419db34957b9ffd1d5f6ba7e08d7dab
quantium <alexcollsoutumuro@gmail.com>
2023-03-05 22:45:34 +0100

    add console

--- app/src/components/ScreenPanel.vue
+++ app/src/components/ScreenPanel.vue
@@ -14,12 +14,11 @@ export default {
 </script>
 <template>
   <div class="m-auto my-8 py-2 w-80 rounded-xl border shadow-inner"
-  :class="store.dark ? `bg-black border-${store.color} shadow-white/20` : 'bg-gray-400'">
-    <div class="py-2 text-center text-2xl text-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-orange-600">
+  :class="store.dark ? `bg-black/50 text-white/80 border-white/30 shadow-${store.color}` : `bg-white/50 text-black/80 border-black/30 shadow-${store.color}`">
+    <div class="py-2 mt-2">
       {{ store.operations }}
     </div>
-    <div class="py-4 text-4xl font-bold"
-    :class="`text-${store.color}`" >
+    <div class="py-4 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-orange-600" >
       <span class="text-md">{{ store.operator }}</span> {{ nf.format(Number(store.number)).replaceAll(',', ' ') }}{{ store.decimals }}
     </div>
   </div>
