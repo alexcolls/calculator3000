ed6ae966e947319a3613311abf1d7c18972de453
quantium <alexcollsoutumuro@gmail.com>
2023-03-05 23:50:30 +0100

    update screen

--- app/src/components/ScreenPanel.vue
+++ app/src/components/ScreenPanel.vue
@@ -16,8 +16,8 @@ export default {
   <div class="my-8">
     <h1 class="p-2 mb-2 text-center uppercase text-sm tracking-widest text-gray-400" 
     :class="`text-${store.color}`">
-          CALCULATOR 3000
-        </h1>
+      CALCULATOR 3000
+    </h1>
     <div class="m-auto py-2 w-80 rounded-xl border shadow-inner"
     :class="store.dark ? `bg-black/50 text-white/80 border-white/30 shadow-${store.color}` : `bg-white/50 text-black/80 border-black/30 shadow-${store.color}`" >
       
