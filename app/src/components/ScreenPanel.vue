35d0dafcaae648a5ba32c95f152c899a49e06cc3
quantium <alexcollsoutumuro@gmail.com>
2023-03-06 00:08:58 +0100

    add parenthesis

--- app/src/components/ScreenPanel.vue
+++ app/src/components/ScreenPanel.vue
@@ -11,6 +11,8 @@ export default {
       store.operations = `${store.operations} ${op} (`;
     }
     function closeParenthesis() {
+      const ops = store.operations.split(' ');
+      if (ops.length > 3)   
       store.operations = store.operations + ' )';
     }
     return {
@@ -30,13 +32,13 @@ export default {
     </h1>
     <div class="m-auto py-2 w-80 rounded-xl border shadow-inner"
     :class="store.dark ? `bg-black/50 text-white/80 border-white/30 shadow-${store.color}` : `bg-white/50 text-black/80 border-black/30 shadow-${store.color}`" >
-      <div>
+      <div class="flex justify-between">
         <button class="ml-3 rounded-full h-6 w-6 m-2 flex justify-center items-center shadow-xl text-xs font-bold" 
         @click="openParenthesis()" 
         :class="store.dark ? 'bg-white/10 shadow-gray-700 border-white/20 hover:bg-gray-600 text-white' : 'bg-white hover:bg-gray-200 border-black/20 text-gray-600'">
           (
         </button>
-        <button class="ml-3 rounded-full h-6 w-6 m-2 flex justify-center items-center shadow-xl text-xs font-bold" 
+        <button class="mr-3 rounded-full h-6 w-6 m-2 flex justify-center items-center shadow-xl text-xs font-bold" 
           @click="openParenthesis()" 
           :class="store.dark ? 'bg-white/10 shadow-gray-700 border-white/20 hover:bg-gray-600 text-white' : 'bg-white hover:bg-gray-200 border-black/20 text-gray-600'">
           )
@@ -48,6 +50,18 @@ export default {
       <div class="py-2 mb-2 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r" 
       :class="`from-${store.color} to-orange-600`" >
         <span class="text-md">{{ store.operator }}</span> {{ nf.format(Number(store.number)).replaceAll(',', ' ') }}{{ store.decimals }}
+      </div>
+      <div class="flex justify-between">
+        <button class="ml-3 rounded-full h-6 w-6 m-2 flex justify-center items-center shadow-xl text-xs font-bold" 
+        @click="openParenthesis()" 
+        :class="store.dark ? 'bg-white/10 shadow-gray-700 border-white/20 hover:bg-gray-600 text-white' : 'bg-white hover:bg-gray-200 border-black/20 text-gray-600'">
+          (
+        </button>
+        <button class="mr-3 rounded-full h-6 w-6 m-2 flex justify-center items-center shadow-xl text-xs font-bold" 
+          @click="openParenthesis()" 
+          :class="store.dark ? 'bg-white/10 shadow-gray-700 border-white/20 hover:bg-gray-600 text-white' : 'bg-white hover:bg-gray-200 border-black/20 text-gray-600'">
+          )
+        </button>
       </div>
     </div>
   </div>
