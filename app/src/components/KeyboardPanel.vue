6a3b68f0b08e2392f5ca688880750de49cdc66e8
quantium <alexcollsoutumuro@gmail.com>
2023-03-09 12:48:13 +0100

    add keyboard functionality

--- app/src/components/KeyboardPanel.vue
+++ app/src/components/KeyboardPanel.vue
@@ -17,7 +17,39 @@ export default {
     function playSuccess(): void { if (store.sound) success_.play(); }
     function playError(): void { if (store.sound) error_.play(); }
     // Keyboard functionality
+    window.addEventListener('keypress', e => {
+      console.log(e.key)
+      if (e.key === 'Enter') {
+        calculateResult();
+        return;
+      } else if (e.key === 'Delete') {
+        clickDEL();
+        return;
+      }
+      const key = String.fromCharCode(e.keyCode);
+      if (key === '=') {
+        calculateResult();
+      } else if (key === '.' || key === ',') {
+        clickDecimals();
+      } else if (!isNaN(Number(key))) {
+        clickNum(Number(key));
+      } else if (['+', '-'].includes(key)) {
+        clickOperator(key);
+      } else if (key === 'x' || key === '*') {
+        clickOperator('x');
+      } else if (key === '/' || key === ':' || key === '÷') {
+        clickOperator('÷');
+      } else if (key === 'e' || key === '^' || key === 'ⁿ') {
+        clickOperator('×ⁿ');
+      } else if (key === 'r' || key === 's' || key === '√') {
+        clickOperator('√');
+      } else {
+        clickAC();
+      }
+      console.log(key);
+    });
     function clickNum(n: number): void {
+      playBeep();
       store.animate = false;
       store.message = '';
       store.idx = 0;
@@ -44,7 +76,20 @@ export default {
         }
       }
     }
+    function clickDecimals(): void {
+      playBeep();
+      store.animate = false;
+      if (store.operator === '0') return;
+      if (store.operator === '=') {
+        clickAC();
+        return;
+      }
+      if (store.decimals) store.decimals = '';
+      else store.decimals = '.';
+      store.updateConsole();
+    }
     function clickOperator(op: string): void {
+      playBeep();
       store.animate = false;
       store.message = '';
       store.idx = 0;
@@ -65,23 +110,13 @@ export default {
         resetNum();
       }
     }
-    function clickDecimals(): void {
-      store.animate = false;
-      if (store.operator === '0') return;
-      if (store.operator === '=') {
-        clickAC();
-        return;
-      }
-      if (store.decimals) store.decimals = '';
-      else store.decimals = '.';
-      store.updateConsole();
-    }
     function resetNum(): void {
       store.number = '0';
       store.decimals = '';
       store.updateConsole();
     }
     function clickDEL(): void {
+      if (store.console === '0') return;
       store.animate = false;
       store.message = 'DEL 💀';
       store.idx = 0;
@@ -124,7 +159,7 @@ export default {
     function calculateResult(): void {
       store.animate = false;
       if (store.operator === '=' 
-       || (!store.operations && store.number === '0')) {
+       || (!store.operations && store.console === '0')) {
         playBeep();
         clickAC();
         return;
@@ -137,9 +172,6 @@ export default {
         .replace('÷', '/')
         .replace('xⁿ', '**')
         .replace(/√\s*(\d+)/g, 'Math.sqrt($1)');
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      // const Parser = require('expr-eval').Parser;
-      // const parser = new Parser();
       const result = eval(operations);
       const total = Math.round((result + Number.EPSILON) * Math.pow(10, maxDecimals)) / Math.pow(10, maxDecimals);
       const totalSpit = String(total).split('.');
@@ -163,12 +195,6 @@ export default {
       store.message = randomMsgs[idx];
       return;
     }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    function handleKeyDown(event: any) {
-      if (event.keyCode >= 48 && event.keyCode <= 57) {
-        clickNum(event.keyCode);
-      }
-    }
     return {
       store,
       playBeep,
@@ -178,129 +204,127 @@ export default {
       clickDEL,
       clickAC,
       calculateResult,
-      handleKeyDown
     };
   }
 }
 </script>
 <template>
   <div class="m-auto w-80">
-    <div @keydown="handleKeyDown"></div>
     <div class="grid grid-cols-5 gap-1 text-s font-semibold text-center rounded-xl">
       <!-- 1 -->
-      <button @click="[clickNum(1), playBeep()]" 
+      <button @click="clickNum(1)" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm rounded-tl-xl">
         1
       </button>
       <!-- 2 -->
-      <button @click="[clickNum(2), playBeep()]" 
+      <button @click="clickNum(2)" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
         2
       </button>
       <!-- 3 -->
-      <button @click="[clickNum(3), playBeep()]" 
+      <button @click="clickNum(3)" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm rounded-tr-xl">
         3
       </button>
       <!-- DEL -->
-      <button @click="[clickDEL(), playBeep()]" 
+      <button @click="clickDEL()" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm rounded-tl-xl text-sm">
         DEL
       </button>
       <!-- AC -->
-      <button @click="[clickAC(), playBeep()]"
+      <button @click="clickAC()"
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm rounded-tr-xl text-sm">
         AC
       </button>
       <!-- 4 -->
-      <button @click="[clickNum(4), playBeep()]" 
+      <button @click="clickNum(4)" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
         4
       </button>
       <!-- 5 -->
-      <button @click="[clickNum(5), playBeep()]" 
+      <button @click="clickNum(5)" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
         5
       </button>
       <!-- 6 -->
-      <button @click="[clickNum(6), playBeep()]" 
+      <button @click="clickNum(6)" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
         6
       </button>
       <!-- + -->
-      <button @click="[clickOperator('+'), playBeep()]" 
+      <button @click="clickOperator('+')" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
         +
       </button>
       <!-- - -->
-      <button @click="[clickOperator('-'), playBeep()]" 
+      <button @click="clickOperator('-')" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
         -
       </button>
       <!-- 7 -->
-      <button @click="[clickNum(7), playBeep()]" 
+      <button @click="clickNum(7)" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
         7
       </button>
       <!-- 8 -->
-      <button @click="[clickNum(8), playBeep()]" 
+      <button @click="clickNum(8)" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
         8
       </button>
       <!-- 9 -->
-      <button @click="[clickNum(9), playBeep()]" 
+      <button @click="clickNum(9)" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
         9
       </button>
        <!-- x -->
-      <button @click="[clickOperator('x'), playBeep()]" 
+      <button @click="clickOperator('x')" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm text-sm">
         x
       </button>
       <!-- ÷ -->
-      <button @click="[clickOperator('÷'), playBeep()]" 
+      <button @click="clickOperator('÷')" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
         ÷
       </button>
       <!-- · -->
-      <button @click="[clickDecimals(), playBeep()]" 
+      <button @click="clickDecimals()" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm rounded-bl-xl">
         ·
       </button>
       <!-- 0 -->
-      <button @click="[clickNum(0), playBeep()]" 
+      <button @click="clickNum(0)" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm">
@@ -308,21 +332,21 @@ export default {
       </button>
       <!-- = -->
       <button @click="calculateResult()"
-      v-on:keyup.enter="[calculateResult(), playBeep()]"
+      v-on:keyup.enter="calculateResult()"
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm rounded-br-xl">
         =
       </button>
       <!-- xⁿ -->
-      <button @click="[clickOperator('×ⁿ'), playBeep()]" 
+      <button @click="clickOperator('×ⁿ')" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm rounded-bl-xl">
         ×ⁿ
       </button>
       <!-- √ -->
-      <button @click="[clickOperator('√'), playBeep()]" 
+      <button @click="clickOperator('√')" 
       :class="[store.dark ? 'bg-gray/900 hover:bg-gray-600 border-gray-500' : 'bg-gray-50 hover:bg-gray-200 border-gray-100',
       `shadow-${store.color}`]" 
       class="py-4 px-2 align-middle relative border shadow-sm rounded-br-xl text-sm">
